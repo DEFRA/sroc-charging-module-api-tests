1d1b26cd4d0c1ce9a65b8d4abeca2bac4a9a993a
Isse <anwar.isse@defra.gov.uk>
2021-11-11 17:25:06 +0000

    Add calculate charge validation tests

--- cypress/endpoints/transaction_endpoints.js
+++ cypress/endpoints/transaction_endpoints.js
@@ -12,6 +12,35 @@ class TransactionEndpoints {
         body
       })
   }
+
+  static calculateInvalid (body) {
+    return cy
+      .api({
+        method: 'POST',
+        url: `/v3/wrls/calculate-charge`,
+        failOnStatusCode: false,
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+          Authorization: `Bearer ${Cypress.env('token')}`
+        },
+        body
+      })
+  }
+
+  static calculate (body) {
+    return cy
+      .api({
+        method: 'POST',
+        url: `/v3/wrls/calculate-charge`,
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+          Authorization: `Bearer ${Cypress.env('token')}`
+        },
+        body
+      })
+  }
 }
 
 export default TransactionEndpoints
