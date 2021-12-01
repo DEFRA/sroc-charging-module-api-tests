cee820d3f97b2fa14af6f863c17343ee7839ce13
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-12-01 01:15:21 +0000

    Update general scenarios
    We now ensure both rulesets are covered by the general scenarios and that we are checking the responses are exactly as expected.

--- cypress/integration/calculate_charge/general.feature
+++ cypress/integration/calculate_charge/general.feature
@@ -3,10 +3,14 @@ Feature: Calculate Charge General
   Background: Authenticate
     Given I am the "system" user
 
-  Scenario: Making a valid request
-    When I make a valid request
-    Then I get a successful response
+  Scenario: Making a valid SROC request
+    When I make a valid sroc request
+    Then I get a successful sroc response
 
-  Scenario: Making an invalid request
-    When I make an invalid request
+  Scenario: Making a valid PRESROC request
+    When I make a valid presroc request
+    Then I get a successful presroc response
+
+  Scenario: Making an invalid SROC request
+    When I make an invalid sroc request
     Then I get a failed response
--- cypress/integration/calculate_charge/general/general_steps.js
+++ cypress/integration/calculate_charge/general/general_steps.js
@@ -1,24 +1,66 @@
 import { When, Then } from 'cypress-cucumber-preprocessor/steps'
 import CalculateChargeEndpoints from '../../../endpoints/calculate_charge_endpoints'
 
-When('I make a valid request', () => {
-  cy.fixture('calculate.presroc.charge').then((calculateCharge) => {
+When('I make a valid {word} request', (ruleset) => {
+  const fixtureName = `calculate.${ruleset}.charge`
+
+  cy.fixture(fixtureName).then((calculateCharge) => {
     CalculateChargeEndpoints.calculate(calculateCharge).then((response) => {
       cy.wrap(response).as('calculateChargeResponse')
     })
   })
 })
 
-Then('I get a successful response', () => {
+Then('I get a successful presroc response', () => {
+  const expectedResult = {
+    calculation: {
+      chargeValue: 2093,
+      sourceFactor: 3,
+      seasonFactor: 1.6,
+      lossFactor: 0.03,
+      licenceHolderChargeAgreement: null,
+      chargeElementAgreement: null,
+      eiucSourceFactor: 0,
+      eiuc: 0,
+      suc: 2751
+    }
+  }
+
   cy.get('@calculateChargeResponse').then((response) => {
     expect(response.status).to.equal(200)
     expect(response.body).to.have.property('calculation')
+
+    expect(JSON.stringify(response.body)).to.equal(JSON.stringify(expectedResult))
   })
 })
 
-When('I make an invalid request', () => {
-  cy.fixture('calculate.presroc.charge').then((calculateCharge) => {
-    calculateCharge.ruleset = 'cors'
+Then('I get a successful sroc response', () => {
+  const expectedResult = {
+    calculation: {
+      chargeValue: 9481,
+      baseCharge: 9700,
+      waterCompanyChargeValue: 0,
+      supportedSourceValue: 0,
+      winterOnlyFactor: null,
+      section130Factor: null,
+      section127Factor: null,
+      compensationChargePercent: null
+    }
+  }
+
+  cy.get('@calculateChargeResponse').then((response) => {
+    expect(response.status).to.equal(200)
+    expect(response.body).to.have.property('calculation')
+
+    expect(JSON.stringify(response.body)).to.equal(JSON.stringify(expectedResult))
+  })
+})
+
+When('I make an invalid {word} request', (ruleset) => {
+  const fixtureName = `calculate.${ruleset}.charge`
+
+  cy.fixture(fixtureName).then((calculateCharge) => {
+    calculateCharge.periodEnd = ''
     CalculateChargeEndpoints.calculate(calculateCharge, false).then((response) => {
       cy.wrap(response).as('calculateChargeResponse')
     })
